5a1233b6c09f492923db16df0d4b29de79beda55
Nurym Kenzh <nak290284@gmail.com>
2019-09-18 10:30:51 +0600

    Chart

--- GeoNodeWeb/Controllers/esnowController.cs
+++ GeoNodeWeb/Controllers/esnowController.cs
@@ -6,11 +6,48 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 using System.Data.SqlClient;
+using Newtonsoft.Json.Linq;
 
 namespace GeoNodeWeb.Controllers
 {
     public class esnowController : Controller
     {
+        enum DataType
+        {
+            MissingData = 0,
+            NoDecision = 1,
+            Night = 11,
+            NoSnow = 25,
+            Lake = 37,
+            Sea = 39,
+            Cloud = 50,
+            LakeIce = 100,
+            Snow = 200,
+            DetectorSaturated = 254,
+            Fill = 255
+        }
+
+        private class SnowData
+        {
+            public DataType DataType;
+            public int PixelsCount;
+            public decimal Area;
+            public decimal Percentage;
+        }
+
+        private class rasterstat
+        {
+            public DateTime date;
+            public string data;
+            public List<SnowData> SnowData;
+            public int PixelsCount;
+            public decimal Area;
+            public rasterstat()
+            {
+                SnowData = new List<SnowData>();
+            }
+        }
+
         public IActionResult Index()
         {
             using (var connection = new NpgsqlConnection("Host=db-geodata.test.geoportal.ingeo.kz;Database=geoserver;Username=postgres;Password=;Port=15433"))
@@ -20,6 +57,70 @@ namespace GeoNodeWeb.Controllers
                 ViewBag.DateTime = datetime.OrderBy(d => d).ToArray();
             }
             return View();
+        }
+
+        public IActionResult Charts(int Id)
+        {
+            ViewBag.Id = Id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetWMAInfo(int Id)
+        {
+            List<rasterstat> rasterstats = new List<rasterstat>();
+            using (var connection = new NpgsqlConnection("Host=db-geodata.test.geoportal.ingeo.kz;Database=postgres;Username=postgres;Password=;Port=15433"))
+            {
+                connection.Open();
+                var rasterstatsC = connection.Query<rasterstat>($"SELECT date, data FROM public.esnow_rasterstats " +
+                    $"WHERE \"feature_id\" = {Id.ToString()};");
+                rasterstats = rasterstatsC.ToList();
+            }
+            foreach (rasterstat rasterstat in rasterstats)
+            {
+                var data = JObject.Parse(rasterstat.data);
+                foreach (JProperty property in data.Properties())
+                {
+                    if (int.TryParse(property.Name, out int n))
+                    {
+                        rasterstat.SnowData.Add(new SnowData()
+                        {
+                            DataType = (DataType)Convert.ToInt32(property.Name),
+                            PixelsCount = Convert.ToInt32(property.Value)
+                        });
+                    }
+                    if (property.Name == "count")
+                    {
+                        rasterstat.PixelsCount = Convert.ToInt32(property.Value);
+                    }
+
+                    if (property.Name == "AreaInSkm")
+                    {
+                        rasterstat.Area = Convert.ToDecimal(property.Value);
+                    }
+                }
+                for (int i = 0; i < rasterstat.SnowData.Count(); i++)
+                {
+                    rasterstat.SnowData[i].Area = rasterstat.Area * rasterstat.SnowData[i].PixelsCount / rasterstat.PixelsCount;
+                    rasterstat.SnowData[i].Percentage = (decimal)rasterstat.SnowData[i].PixelsCount / rasterstat.PixelsCount * 100;
+                }
+            }
+
+            string wmaname = "";
+            using (var connection = new NpgsqlConnection("Host=db-geodata.test.geoportal.ingeo.kz;Database=geoserver;Username=postgres;Password=;Port=15433"))
+            {
+                connection.Open();
+                var name = connection.Query<string>($"SELECT \"NameWMB_Ru\" " +
+                    $"FROM public.wma_polygon_1 " +
+                    $"WHERE fid = 1 " +
+                    $"LIMIT 1;");
+                wmaname = name.FirstOrDefault();
+            }
+            return Json(new
+            {
+                wmaname,
+                rasterstats
+            });
         }
     }
 }
