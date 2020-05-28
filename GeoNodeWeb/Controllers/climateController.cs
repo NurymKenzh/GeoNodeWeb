d883a2c1f22bc0e43e001706035bda9c2cd76fdb
Nurym Kenzh <nak290284@gmail.com>
2020-05-28 14:45:15 +0600

    Translates: deploy

--- GeoNodeWeb/Controllers/climateController.cs
+++ GeoNodeWeb/Controllers/climateController.cs
@@ -14,6 +14,7 @@ using Dapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
 using Newtonsoft.Json;
@@ -139,6 +140,18 @@ namespace GeoNodeWeb.Controllers
             geoportalProdConnection = server ? Startup.Configuration["geoportalProdConnectionServer"].ToString() : Startup.Configuration["geoportalProdConnectionDebug"].ToString(),
 
             geodataanalyticsProdConnection = server ? Startup.Configuration["geodataanalyticsProdConnectionServer"].ToString() : Startup.Configuration["geodataanalyticsProdConnectionDebug"].ToString();
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
 
         public climateController(HttpApiClientController HttpApiClient,
             IHostingEnvironment hostingEnvironment,
