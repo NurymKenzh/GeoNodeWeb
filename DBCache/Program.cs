b3b66a8fb8882e0c0b7985a6cff75aaa2312fa3c
Nurym Kenzh <nak290284@gmail.com>
2020-06-02 10:29:49 +0600

    DBCache

---
+++ DBCache/Program.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Npgsql;
+using System;
+
+namespace DBCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString()} >> starting creating cache!");
+            var connection = new NpgsqlConnection("Host=localhost;Database=analytics;Username=postgres;Password=postgres;Port=5432");
+            connection.Open();
+            string query = "SELECT id, name, dt, point, value FROM public.climate_et;" +
+                "SELECT id, name, dt, point, value FROM public.climate_et_dlt;" +
+                "SELECT id, name, dt, point, value FROM public.climate_gtk;" +
+                "SELECT id, name, dt, point, value FROM public.climate_pr;" +
+                "SELECT id, name, dt, point, value FROM public.climate_pr_dlt;" +
+                "SELECT id, name, dt, point, value FROM public.climate_spei;" +
+                "SELECT id, name, dt, point, value FROM public.climate_spi;" +
+                "SELECT id, name, dt, point, value FROM public.climate_tas;" +
+                "SELECT id, name, dt, point, value FROM public.climate_tas_dlt;" +
+                "SELECT id, name, dt, point, value FROM public.climate_tasmax;" +
+                "SELECT id, name, dt, point, value FROM public.climate_tasmax_dlt;" +
+                "SELECT id, name, dt, point, value FROM public.climate_tasmin;" +
+                "SELECT id, name, dt, point, value FROM public.climate_tasmin_dlt;";
+            connection.Query(query);
+            connection.Close();
+            Console.WriteLine($"{DateTime.Now.ToString()} >> finished creating cache!");
+        }
+    }
+}
