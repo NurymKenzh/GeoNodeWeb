24d1179f762d9ad4ea0221fc403ed3176e8e8c0d
Nurym Kenzh <nak290284@gmail.com>
2020-09-20 03:06:42 +0600

    eraland

---
+++ GeoNodeWeb/Controllers/eralandController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoNodeWeb.Controllers
+{
+    public class eralandController : Controller
+    {
+        public IActionResult Index()
+        {
+            bool server = Convert.ToBoolean(Startup.Configuration["Server"]);
+            ViewBag.GeoServerUrl = server ? Startup.Configuration["GeoServerProdUrlServer"].ToString() : Startup.Configuration["GeoServerProdUrlDebug"].ToString();
+            return View();
+        }
+    }
+}
