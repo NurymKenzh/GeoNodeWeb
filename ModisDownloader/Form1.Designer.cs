6299375a6a5e87234ff388ae58ac0b5aea2c5153
Nurym Kenzh <nak290284@gmail.com>
2021-05-05 15:31:08 +0600

    ModisDownloader

---
+++ ModisDownloader/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace ModisDownloader
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageOptions = new System.Windows.Forms.TabPage();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBoxBoundingBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxArchiveDirectory = new System.Windows.Forms.TextBox();
+            this.tabPageLog = new System.Windows.Forms.TabPage();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.backgroundWorkerDownloader = new System.ComponentModel.BackgroundWorker();
+            this.tabControl1.SuspendLayout();
+            this.tabPageOptions.SuspendLayout();
+            this.tabPageLog.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageOptions);
+            this.tabControl1.Controls.Add(this.tabPageLog);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageOptions
+            // 
+            this.tabPageOptions.Controls.Add(this.buttonStop);
+            this.tabPageOptions.Controls.Add(this.textBoxBoundingBox);
+            this.tabPageOptions.Controls.Add(this.label2);
+            this.tabPageOptions.Controls.Add(this.buttonStart);
+            this.tabPageOptions.Controls.Add(this.label1);
+            this.tabPageOptions.Controls.Add(this.textBoxArchiveDirectory);
+            this.tabPageOptions.Location = new System.Drawing.Point(4, 24);
+            this.tabPageOptions.Name = "tabPageOptions";
+            this.tabPageOptions.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageOptions.Size = new System.Drawing.Size(792, 422);
+            this.tabPageOptions.TabIndex = 0;
+            this.tabPageOptions.Text = "Options";
+            this.tabPageOptions.UseVisualStyleBackColor = true;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(104, 3);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(95, 23);
+            this.buttonStop.TabIndex = 6;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBoxBoundingBox
+            // 
+            this.textBoxBoundingBox.Location = new System.Drawing.Point(110, 75);
+            this.textBoxBoundingBox.Name = "textBoxBoundingBox";
+            this.textBoxBoundingBox.Size = new System.Drawing.Size(334, 23);
+            this.textBoxBoundingBox.TabIndex = 5;
+            this.textBoxBoundingBox.Text = "50,40,80,50";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Bounding box:";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(3, 3);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(95, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Archive directory:";
+            // 
+            // textBoxArchiveDirectory
+            // 
+            this.textBoxArchiveDirectory.Location = new System.Drawing.Point(110, 36);
+            this.textBoxArchiveDirectory.Name = "textBoxArchiveDirectory";
+            this.textBoxArchiveDirectory.Size = new System.Drawing.Size(334, 23);
+            this.textBoxArchiveDirectory.TabIndex = 3;
+            this.textBoxArchiveDirectory.Text = "D:\\MODIS\\Archive";
+            // 
+            // tabPageLog
+            // 
+            this.tabPageLog.Controls.Add(this.textBoxLog);
+            this.tabPageLog.Location = new System.Drawing.Point(4, 24);
+            this.tabPageLog.Name = "tabPageLog";
+            this.tabPageLog.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageLog.Size = new System.Drawing.Size(792, 422);
+            this.tabPageLog.TabIndex = 1;
+            this.tabPageLog.Text = "Log";
+            this.tabPageLog.UseVisualStyleBackColor = true;
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxLog.Location = new System.Drawing.Point(3, 3);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxLog.Size = new System.Drawing.Size(786, 416);
+            this.textBoxLog.TabIndex = 0;
+            this.textBoxLog.WordWrap = false;
+            // 
+            // backgroundWorkerDownloader
+            // 
+            this.backgroundWorkerDownloader.WorkerSupportsCancellation = true;
+            this.backgroundWorkerDownloader.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerDownloader_DoWork);
+            this.backgroundWorkerDownloader.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerDownloader_RunWorkerCompleted);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "FormMain";
+            this.Text = "MODIS Downloader";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageOptions.ResumeLayout(false);
+            this.tabPageOptions.PerformLayout();
+            this.tabPageLog.ResumeLayout(false);
+            this.tabPageLog.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageOptions;
+        private System.Windows.Forms.TabPage tabPageLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxArchiveDirectory;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxBoundingBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonStop;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerDownloader;
+    }
+}
+
---
+++ ModisDownloader/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModisDownloader
+{
+    public partial class FormMain : Form
+    {
+        public class ModisProduct
+        {
+            public string Source;
+            public string Product;
+            public DateTime StartDate;
+            public string[] DataSets;
+            public int[] ExtractDataSetIndexes;
+            public bool Spans;
+            public bool Mosaic;
+            public bool ConvertHdf;
+            public bool Norm = false;
+            public bool Publish;
+            public int? AnomalyStartYear;
+            public int? AnomalyEndYear;
+            public bool Analize = false;
+            public bool ZonalStatRaster = false;
+            public int[] DayDividedDataSetIndexes;
+            public int Period;
+            public string GetProductWithoutVersion()
+            {
+                return Product.Split('.')[0];
+            }
+        }
+
+        //static ModisProduct[] modisProducts = new ModisProduct[5];
+        List<ModisProduct> modisProducts = new List<ModisProduct>();
+
+        public FormMain()
+        {
+            InitializeComponent();
+            modisProducts.Add(new ModisProduct()
+            {
+                Source = "SAN/MOST",
+                Product = "MOD10A1.006",
+                StartDate = new DateTime(2000, 2, 24),
+                Period = 1,
+                DataSets = new string[7]
+                {
+                    "NDSISnowCover",
+                    "NDSISnowCoverBasic",
+                    "NDSISnowCoverAlgorithm",
+                    "NDSI",
+                    "SnowAlbedo",
+                    "orbitpnt",
+                    "granulepnt"
+                },
+                ExtractDataSetIndexes = new int[1] { 0 },
+                Spans = true,
+                Mosaic = true,
+                ConvertHdf = false,
+                Publish = false,
+                Analize = true,
+                ZonalStatRaster = true,
+                DayDividedDataSetIndexes = new int[] { },
+                Norm = false,
+                AnomalyStartYear = null,
+                AnomalyEndYear = null
+            });
+            modisProducts.Add(new ModisProduct()
+            {
+                Source = "SAN/MOSA",
+                Product = "MYD10A1.006",
+                StartDate = new DateTime(2002, 7, 4),
+                Period = 1,
+                DataSets = new string[7]
+                {
+                    "NDSISnowCover",
+                    "NDSISnowCoverBasic",
+                    "NDSISnowCoverAlgorithm",
+                    "NDSI",
+                    "SnowAlbedo",
+                    "orbitpnt",
+                    "granulepnt"
+                },
+                ExtractDataSetIndexes = new int[1] { 0 },
+                Spans = true,
+                Mosaic = true,
+                ConvertHdf = false,
+                Publish = false,
+                Analize = true,
+                ZonalStatRaster = true,
+                DayDividedDataSetIndexes = new int[] { },
+                Norm = false,
+                AnomalyStartYear = null,
+                AnomalyEndYear = null
+            });
+            modisProducts.Add(new ModisProduct()
+            {
+                Source = "SAN/MOST",
+                Product = "MOD10A2.006",
+                StartDate = new DateTime(2000, 2, 24),
+                Period = 8,
+                DataSets = new string[2]
+                {
+                    "MaxSnowExtent",
+                    "SnowCover"
+                },
+                ExtractDataSetIndexes = new int[2] { 0, 1 },
+                Spans = true,
+                Mosaic = true,
+                ConvertHdf = false,
+                Publish = true,
+                Analize = true,
+                ZonalStatRaster = true,
+                DayDividedDataSetIndexes = new int[1] { 1 },
+                Norm = false,
+                AnomalyStartYear = null,
+                AnomalyEndYear = null
+            });
+            modisProducts.Add(new ModisProduct()
+            {
+                Source = "SAN/MOSA",
+                Product = "MYD10A2.006",
+                StartDate = new DateTime(2002, 7, 4),
+                Period = 8,
+                DataSets = new string[2]
+                {
+                    "MaxSnowExtent",
+                    "SnowCover"
+                },
+                ExtractDataSetIndexes = new int[2] { 0, 1 },
+                Spans = true,
+                Mosaic = true,
+                ConvertHdf = false,
+                Publish = true,
+                Analize = true,
+                ZonalStatRaster = true,
+                DayDividedDataSetIndexes = new int[1] { 1 },
+                Norm = false,
+                AnomalyStartYear = null,
+                AnomalyEndYear = null
+            });
+            modisProducts.Add(new ModisProduct()
+            {
+                Source = "SAN/MOST",
+                Product = "MOD10C2.006",
+                StartDate = new DateTime(2000, 2, 24),
+                Period = 8,
+                DataSets = new string[1]
+                {
+                    "NDSI"
+                },
+                ExtractDataSetIndexes = new int[1] { 0 },
+                Spans = false,
+                Mosaic = false,
+                ConvertHdf = true,
+                Publish = true,
+                Analize = true,
+                ZonalStatRaster = false,
+                DayDividedDataSetIndexes = new int[] { },
+                Norm = true,
+                AnomalyStartYear = 2001,
+                AnomalyEndYear = 2019
+            });
+        }
+
+        //bool download = false;
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Log("Download started!");
+            //download = true;
+            backgroundWorkerDownloader.RunWorkerAsync();
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            //download = false;
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+            backgroundWorkerDownloader.CancelAsync();
+        }
+
+        private void backgroundWorkerDownloader_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (!backgroundWorkerDownloader.CancellationPending)
+            {
+                StartDownloadAll();
+            }
+        }
+
+        public void StartDownloadAll()
+        {
+            DateTime dateMin = new DateTime(2100, 12, 31);
+            ModisProduct modisProductToDownload = new ModisProduct();
+            foreach (ModisProduct modisProduct in modisProducts)
+            {
+                DateTime dateCurrent = GetProductLastDate(modisProduct);
+                if (dateCurrent < dateMin)
+                {
+                    dateMin = dateCurrent;
+                    modisProductToDownload = modisProduct;
+                }
+            }
+            StartDownloadProduct(modisProductToDownload);
+        }
+
+        public DateTime GetProductLastDate(ModisProduct ModisProduct)
+        {
+            string dateFile = Path.Combine(textBoxArchiveDirectory.Text, ModisProduct.GetProductWithoutVersion());
+            dateFile = Path.ChangeExtension(dateFile, "txt");
+            if (!File.Exists(dateFile))
+            {
+                return ModisProduct.StartDate.AddDays(-1);
+            }
+            string s_lastDate = File.ReadAllText(dateFile),
+                s_year = s_lastDate.Split('-')[0],
+                s_month = s_lastDate.Split('-')[1],
+                s_day = s_lastDate.Split('-')[2];
+            int year = Convert.ToInt32(s_year),
+                month = Convert.ToInt32(s_month),
+                day = Convert.ToInt32(s_day);
+            return new DateTime(year, month, day);
+        }
+
+        public void StartDownloadProduct(ModisProduct ModisProduct)
+        {
+            DateTime dateLast = GetProductLastDate(ModisProduct),
+                dateDownload = dateLast.AddDays(ModisProduct.Period);
+            DownloadProduct(ModisProduct, dateDownload);
+        }
+
+        public void DownloadProduct(ModisProduct ModisProduct, DateTime Date)
+        {
+            Log($"{ModisProduct.GetProductWithoutVersion()} {Date.ToString("yyyy-MM-dd")} download started!");
+            ClearBufer();
+            bool ok = false;
+            while (!ok)
+            {
+                try
+                {
+                    string resource = $"https://n5eil02u.ecs.nsidc.org/egi/request?" +
+                            $"short_name={ModisProduct.GetProductWithoutVersion()}&" +
+                            $"version=6&" +
+                            $"time={Date.ToString("yyyy-MM-dd")},{Date.ToString("yyyy-MM-dd")}&" +
+                            $"bounding_box={textBoxBoundingBox.Text}&" +
+                            $"agent=NO&" +
+                            $"page_size=2000",
+                        urs = "https://urs.earthdata.nasa.gov",
+                        username = "sandugash_2004",
+                        password = "Arina2009";
+                    CookieContainer myContainer = new CookieContainer();
+                    CredentialCache cache = new CredentialCache();
+                    cache.Add(new Uri(urs), "Basic", new NetworkCredential(username, password));
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(resource);
+                    request.Method = "GET";
+                    request.Credentials = cache;
+                    request.CookieContainer = myContainer;
+                    request.PreAuthenticate = false;
+                    request.AllowAutoRedirect = true;
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    long length = response.ContentLength;
+                    string type = response.ContentType;
+                    Stream stream = response.GetResponseStream();
+                    string fileName = response.Headers["Content-Disposition"].Replace("attachment; filename=", String.Empty).Replace("\"", String.Empty);
+                    using (Stream s = File.Create(Path.Combine(textBoxArchiveDirectory.Text, "Bufer", fileName)))
+                    {
+                        stream.CopyTo(s);
+                    }
+                    stream.Close();
+                    MoveFilesToArchive();
+                    SaveProductLastDate(ModisProduct, Date);
+                    ok = true;
+                }
+                catch (WebException webException)
+                {
+                    if ((HttpWebResponse)webException.Response != null)
+                    {
+                        if (((HttpWebResponse)webException.Response).StatusCode == HttpStatusCode.NotImplemented)
+                        {
+                            Log($"{ModisProduct.GetProductWithoutVersion()} {Date.ToString("yyyy-MM-dd")} download error: " +
+                                $"{((HttpWebResponse)webException.Response).StatusCode} ({((HttpWebResponse)webException.Response).StatusDescription})");
+                            SaveProductLastDate(ModisProduct, Date);
+                            ok = true;
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Log($"{ModisProduct.GetProductWithoutVersion()} {Date.ToString("yyyy-MM-dd")} download error: {exception.Message}");
+                }
+            }
+            Log($"{ModisProduct.GetProductWithoutVersion()} {Date.ToString("yyyy-MM-dd")} download finished!");
+        }
+
+        public void MoveFilesToArchive()
+        {
+            Unzip();
+            foreach (string file in Directory.EnumerateFiles(Path.Combine(textBoxArchiveDirectory.Text, "Bufer"), "*.hdf*", SearchOption.AllDirectories))
+            {
+                string product = Path.GetFileNameWithoutExtension(file).Split('.')[0],
+                    year = Path.GetFileNameWithoutExtension(file).Split('.')[1].Substring(1, 4),
+                    archiveDirecory = Path.Combine(textBoxArchiveDirectory.Text, $"{year}.{product}");
+                if (!Directory.Exists(archiveDirecory))
+                {
+                    Directory.CreateDirectory(archiveDirecory);
+                }
+                File.Move(file, Path.Combine(archiveDirecory, Path.GetFileName(file)));
+            }
+            ClearBufer();
+        }
+
+        public void Unzip()
+        {
+            foreach (string zip in Directory.GetFiles(Path.Combine(textBoxArchiveDirectory.Text, "Bufer"), "*.zip"))
+            {
+                System.IO.Compression.ZipFile.ExtractToDirectory(zip, Path.Combine(textBoxArchiveDirectory.Text, "Bufer"));
+            }
+        }
+
+        public void ClearBufer()
+        {
+            DirectoryInfo di = new DirectoryInfo(Path.Combine(textBoxArchiveDirectory.Text, "Bufer"));
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+
+        public void SaveProductLastDate(ModisProduct ModisProduct, DateTime Date)
+        {
+            string dateFile = Path.Combine(textBoxArchiveDirectory.Text, ModisProduct.GetProductWithoutVersion());
+            dateFile = Path.ChangeExtension(dateFile, "txt");
+            File.WriteAllText(dateFile, Date.ToString("yyyy-MM-dd"));
+        }
+
+        public void Log(string Message)
+        {
+            //textBoxLog.AppendText($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} >> {Message}{Environment.NewLine}");
+            Action action = () => textBoxLog.AppendText($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} >> {Message}{Environment.NewLine}");
+            textBoxLog.Invoke(action);
+        }
+
+        private void backgroundWorkerDownloader_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Log("Download stopped!");
+        }
+    }
+}
